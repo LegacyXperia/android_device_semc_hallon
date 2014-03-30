162a31f27c67dd7d7721a639ba3feddede234fac
Michael Bestas <mikeioannina@gmail.com>
2014-03-30 22:35:05 +0300

    hallon: Use new defconfig
    Change-Id: Ic221b1c42376a1170d334c21256503d96560fdb5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/semc/hallon/include
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/hallon/bluedroid
 
 # Kernel
-TARGET_KERNEL_CONFIG := nAa_hallon_defconfig
+TARGET_KERNEL_CONFIG := lx_hallon_defconfig
 
 # Sensors
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
